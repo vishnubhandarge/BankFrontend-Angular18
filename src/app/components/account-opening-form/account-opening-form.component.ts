c9733f8cabb173a39402991a20e16f288d7392b1
Bittu <bittu1040@gmail.com>
2024-10-20 20:52:27 +0530

    form validation

--- src/app/components/account-opening-form/account-opening-form.component.ts
+++ src/app/components/account-opening-form/account-opening-form.component.ts
@@ -1,67 +1,57 @@
-import { JsonPipe } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
-import { AbstractControl, ValidatorFn } from '@angular/forms';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'; import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'; import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-account-opening-form',
-  standalone: true, imports: [RouterLink, ReactiveFormsModule, FormsModule],
+  standalone: true, 
+  imports: [RouterLink, ReactiveFormsModule, FormsModule, NgIf],
   templateUrl: './account-opening-form.component.html',
   styleUrl: './account-opening-form.component.css'
 })
 
 export class AccountOpeningFormComponent implements OnInit {
-  accountForm: FormGroup;
+  accountForm: FormGroup = new FormGroup({});
+  http = inject(HttpClient);
+
   constructor(private fb: FormBuilder) {
-    this.accountForm = this.fb.group({
-      firstName: ['', [Validators.required, Validators.maxLength(50)]],
-      lastName: ['', [Validators.required, Validators.maxLength(50)]],
-      birthDate: ['', Validators.required],
-      mobile: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
-      email: ['', [Validators.required, Validators.email, Validators.maxLength(100)]],
-      houseNo: ['', Validators.required],
-      addressLine1: ['', [Validators.required, Validators.maxLength(100)]],
-      addressLine2: ['', Validators.maxLength(100)],
-      taluka: ['', Validators.required],
-      city: ['', Validators.required],
-      state: ['', Validators.required],
-      country: ['', Validators.required],
-      pinCode: ['', [Validators.required, Validators.pattern(/^\d{6}$/)]],
-      nomineeName: ['', Validators.required],
-      relationWithNominee: ['', Validators.required],
-      nomineeDOB: ['', Validators.required],
-      accountType: ['', Validators.required]
-    });
+
   }
-  ngOnInit(): void { }
-  openAccountObj: any =
-    {
-      "firstName": "",
-      "lastName": "",
-      "birthDate": "",
-      "mobile": "",
-      "email": "",
-      "houseNo": "",
-      "addressLine1": "",
-      "addressLine2": "",
-      "taluka": "",
-      "city": "",
-      "state": "",
-      "country": "",
-      "pinCode": "",
-      "nomineeName": "",
-      "relationWithNominee": "",
-      "nomineeDOB": "",
-      "accountType": ""
-    };
-  http = inject(HttpClient); 
+
+  ngOnInit(): void {
+      this.accountForm = this.fb.group({
+        firstName: ['test', [Validators.required, Validators.maxLength(50)]],
+        lastName: ['test', [Validators.required, Validators.maxLength(50)]],
+        birthDate: ['2024-10-22', Validators.required],
+        mobile: ['1234567890', [Validators.required, Validators.pattern('[0-9]{10}')]],
+        email: ['abc@abc.com', [Validators.required, Validators.email, Validators.maxLength(100)]],
+        houseNo: ['12', Validators.required],
+        addressLine1: ['abc', [Validators.required, Validators.maxLength(100)]],
+        addressLine2: ['abc', Validators.maxLength(100)],
+        taluka: ['Latur', Validators.required],
+        city: ['Latur', Validators.required],
+        state: ['Maharashtra', Validators.required],
+        country: ['India', Validators.required],
+        pinCode: ['123456', [Validators.required, Validators.pattern('[0-9]{6}'), Validators.minLength(6)]],
+        nomineeName: ['abc', Validators.required],
+        relationWithNominee: ['Sister', Validators.required],
+        nomineeDOB: ['2024-10-21', Validators.required],
+        accountType: ['Savings', Validators.required]
+      });
+  }
+
   onSubmit() {
-    this.http.post("https://localhost:7093/api/Account/OpenAccount", this.openAccountObj).subscribe((res: any) => {
-      if (res.result) {
-        alert("Account opened successfullt");
-      } else {
-        alert("Account opening failed");
-      }
-    });
+    if (this.accountForm.valid) {
+      console.log(this.accountForm.value);
+      this.http.post("https://localhost:7093/api/Account/OpenAccount", this.accountForm.value).subscribe((res: any) => {
+        if (res.result) {
+          alert("Account opened successfullt");
+        } else {
+          alert("Account opening failed");
+        }
+      });
+    } else {
+      console.error('Form is invalid');
+    }
   }
 }
